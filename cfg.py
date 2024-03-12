2778cefe5e5ec023fbffc68f7404075d81585569
Devansh Tayal <devliveforever@gmail.com>
2024-03-12 02:27:32 -0400

    files+badnet

---
+++ cfg.py
@@ -0,0 +1,6 @@
+import os
+
+base_path = f"{os.getenv('HOME')}/Desktop/ILM-VP/FLM-VP-backdoors"
+
+data_path = os.path.join(base_path, "data")
+results_path = os.path.join(base_path, "projects", "ILM-VP", "FLM-VP-backdoors", "results")
